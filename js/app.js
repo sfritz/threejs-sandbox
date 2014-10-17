eb4523466af3008acdb8bcf398793147f823ef07
sfritz <scottgfritz@gmail.com>
2014-10-16 23:42:55 -0500

    Ballooning sphere

--- js/app.js
+++ js/app.js
@@ -1,23 +1,51 @@
+var VIEW_ANGLE = 45,
+    WIDTH = window.innerWidth,
+    HEIGHT = window.innerHeight,
+    ASPECT = WIDTH / HEIGHT,
+    NEAR = 0.1,
+    FAR = 1000;
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera(
-  75, window.innerWidth / window.innerHeight, 0.1, 1000
+  VIEW_ANGLE,
+  ASPECT,
+  NEAR,
+  FAR
 );
+scene.add(camera);
+camera.position.z = 300;
+
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setSize(WIDTH, HEIGHT);
 document.body.appendChild(renderer.domElement);
 
-var geometry = new THREE.BoxGeometry(1,1,1);
-var material = new THREE.MeshBasicMaterial({
-  color: 0x00ff00
-});
-var cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-camera.position.z = 5;
+var radius = 50,
+    segments = 16,
+    rings = 16;
+var sphere = new THREE.Mesh(
+  new THREE.SphereGeometry(
+    radius,
+    segments,
+    rings
+  ),
+  new THREE.MeshLambertMaterial({
+    color: 0xCC0000
+  })
+);
+scene.add(sphere);
 
+var pointLight = new THREE.PointLight(0xFFFFFF);
+pointLight.position.x = 10;
+pointLight.position.y = 50;
+pointLight.position.z = 130;
+scene.add(pointLight);
+
+var frames = 0;
 function render() {
   requestAnimationFrame(render);
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.1;
+  scale = (Math.sin(frames/20)+1.3)/2;
+  sphere.scale.set(scale, scale, scale);
   renderer.render(scene, camera);
+  frames++;
 }
 render();
